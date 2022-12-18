da4ff01318738c054109701b6f56a1fda1184241
MaksymilianBilski <biger9@o2.pl>
2022-12-18 15:36:15 +0100

    first attempt

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+
+import React, { Component } from "react";
+
+export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      </div >
+    );
+  }
+};
